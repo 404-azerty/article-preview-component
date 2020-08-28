7abc6281b2377748360aa67ca1ed9592b4ed97c5
Johanne <johannemay@icloud.com>
2020-08-28 10:34:35 +0200

    js effect ok

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+// Detect click on share button
+const footerLinks = document.querySelector('.footer-links');
+const footerWriter = document.querySelector('.footer-writer');
+function toggle() {
+  footerLinks.classList.toggle('active');
+  footerWriter.classList.toggle('active');
+}
